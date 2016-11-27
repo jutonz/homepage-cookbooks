ad90cb230434c764108f419ea97a57c16d77d561
Justin Toniazzo <jutonz42@gmail.com>
2016-11-26 23:52:37 -0500

    initial

---
+++ cookbooks/recipes/default.rb
@@ -0,0 +1,13 @@
+data_dir = value_for_platform(
+  "centos" => { "default" => "/srv/www/shared" },
+  "ubuntu" => { "default" => "/srv/www/data" },
+  "default" => "/srv/www/config"
+)
+
+directory data_dir do
+  mode 0755
+  owner 'root'
+  group 'root'
+  recursive true
+  action :create
+end
